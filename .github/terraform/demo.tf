3e65a6cdbd1ae47692e711eb9df353875b30aa05
Shema Christophe <jeanchristopheshema@gmail.com>
2022-08-21 20:55:20 -0700

    new changes

---
+++ .github/terraform/demo.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  version = "~>2.0"
+  region  = "us-west-1"
+}
+terraform {
+  backend "s3" {
+    bucket = "shema-remote-state-bucket"
+    key    = "shema-remote-state-bucket"
+    region = "us-west-1"
+  }
+}
+
+resource "aws_s3_bucket" "s3Bucket" {
+  bucket = "shema-cool-tf-bucket"
+  acl    = "public-read"
+
+  policy = <<EOF
+{
+  "Id": "MakePublic",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::shema-cool-tf-bucket/*",
+      "Principal": "*"
+    }
+  ]
+}
+EOF
+
+  website {
+    index_document = "index.html"
+  }
+}
