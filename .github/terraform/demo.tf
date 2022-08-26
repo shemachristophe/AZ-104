c88a8546bf9cb8d82081a2a71f0e079ceb898a89
Shema Christophe <75677615+shemachristophe@users.noreply.github.com>
2022-08-26 00:19:42 -0700

    Update demo.tf

--- .github/terraform/demo.tf
+++ .github/terraform/demo.tf
@@ -2,6 +2,7 @@ terraform {
   required_version = "~> 1.0"
   backend "s3" {
     bucket = "shema-remote2-state-bucket"
+    key = "shema-remote2-state-bucket"
     region = "us-west-1"
     encrypt = true
   }
