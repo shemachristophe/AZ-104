592aafe580c9a4726658c88fea7f2cdff71fb793
Shema Christophe <75677615+shemachristophe@users.noreply.github.com>
2022-08-29 15:30:01 -0700

    Update main.tf

--- .github/terraform/main.tf
+++ .github/terraform/main.tf
@@ -18,16 +18,3 @@ provider "aws" {
   region = var.region
 
 }
-
-resource "aws_s3_bucket" "s3Bucket" {
-  bucket = "shema-cool-tf-bucket"
-  tags = {
-    Name = "Shema Bucket"
-    Environment = "dev"
-  }
-}
-
-resource "aws_s3_bucket_acl" "shema-cool-tf-bucket-acl" {
-  bucket = aws_s3_bucket.s3Bucket.id
-  acl    = "private"
-}
