35f6d105c41eb0a56e909a25d60af6c7353d8d04
Shema Christophe <75677615+shemachristophe@users.noreply.github.com>
2022-08-29 23:12:07 -0700

    Update s3.tf

--- .github/terraform/s3.tf
+++ .github/terraform/s3.tf
@@ -8,5 +8,5 @@ resource "aws_s3_bucket" "s3Bucket" {
 
 resource "aws_s3_bucket_acl" "shema-cool-tf-bucket-acl" {
   bucket = aws_s3_bucket.s3Bucket.id
-  acl    = "bucket-owner-full-control"
+  acl    = "public-read-write"
 }
