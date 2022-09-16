a81263d2f4cc2e99df2a8809a96de598a7d43d50
Shema Christophe <75677615+shemachristophe@users.noreply.github.com>
2022-09-16 08:44:31 -0700

    Create run_plan.sh

---
+++ .github/workflows/run_plan.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+function get_terraform_plan_return_message(){
+  terraform plan -detailed-exitcode
+  exitCode=$?
+  
+  if[ exitCode -eq 2]
+  then
+    echo "Changes Detected!"
+  elif [exitCode -eq 1]
+  then
+    echo "Error Detected!"
+  else
+    echo "No Changes Detected!"
+  fi
+}
