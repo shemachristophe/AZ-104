92c457005fca4b9460ad6940bd4343cced5e6b5a
Shema Christophe <75677615+shemachristophe@users.noreply.github.com>
2022-09-16 09:42:00 -0700

    Update run_plan.sh

--- .github/workflows/run_plan.sh
+++ .github/workflows/run_plan.sh
@@ -13,3 +13,17 @@ function get_terraform_plan_return_message(){
     echo 'No Changes Detected!'
   fi
 }
+
+
+function name_func(){
+  1+1
+  echo $?
+  exitCode=$?
+  if [ $exitCode -eq 2 ]; then
+    echo 'Changes Detected!'
+  elif [ $exitCode -eq 1 ]; then
+    echo 'Error Detected!'
+  else
+    echo 'No Changes Detected!'
+  fi
+}
