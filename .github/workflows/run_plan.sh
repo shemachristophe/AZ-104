c520d043ab55f43262b22cb52915aa65af364448
Shema Christophe <75677615+shemachristophe@users.noreply.github.com>
2022-09-16 09:50:30 -0700

    Update run_plan.sh

--- .github/workflows/run_plan.sh
+++ .github/workflows/run_plan.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
 
 function get_terraform_plan_return_message(){
-  terraform plan -detailed-exitcode
-  echo $?
-  exitCode=$?
+  exitCode=$((terraform plan -detailed-exitcode))
+  #echo $?
+  #exitCode=$?
   
   if [ $exitCode -eq 2 ]; then
     echo 'Changes Detected!'
