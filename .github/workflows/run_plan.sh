81651c9b8863d035aa43dde93a6069e94720e56f
Shema Christophe <75677615+shemachristophe@users.noreply.github.com>
2022-09-17 10:06:25 -0700

    Update run_plan.sh

--- .github/workflows/run_plan.sh
+++ .github/workflows/run_plan.sh
@@ -5,13 +5,14 @@ function get_terraform_plan_return_message(){
   #exitCode='$(terraform plan -detailed-exitcode)'
   #terraform plan -detailed-exitcode -out changes.json
   #terraform plan -detailed-exitcode -out plan.tfplan &> plan.out
-  terraform plan -detailed-exitcode -out plan.out 1>stdout 2>err.txt 
+  terraform plan -detailed-exitcode -no-color > plan.txt 1>stdout 2>err.txt 
   #echo 'err' &2
   read stdout
-  #sed -i 's/Error: Terraform exited with code 2./State Change Detected! Terraform exited with code 2/' stdout
+  sed -i 's/Error: Terraform exited with code 2./State Change Detected! Terraform exited with code 2/' plan.txt
+  cat plan.txt
   exitCode=$?
   #cat stdout
-  terraform show -json plan.out
+  #terraform show -json plan.out
   #echo'_------------------_'
   #cat stderr
   echo 'exitCode is: ' $exitCode
