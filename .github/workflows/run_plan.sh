b4d925fb004051812d8ff0e037bd4685d0193f89
Shema Christophe <75677615+shemachristophe@users.noreply.github.com>
2022-09-16 19:51:01 -0700

    Update run_plan.sh

--- .github/workflows/run_plan.sh
+++ .github/workflows/run_plan.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/env bash
 
 function get_terraform_plan_return_message(){
-  exitCode='$(terraform plan -detailed-exitcode)'
+  #exitCode='$(terraform plan -detailed-exitcode)'
   terraform plan -detailed-exitcode
+  exitCode=$?
   echo 'exitCode is: ' $exitCode
   echo '$? is: ' $?
   #exitCode=$?
