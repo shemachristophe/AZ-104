973087496a8e25e15217d5399453f08c114741a9
Shema Christophe <75677615+shemachristophe@users.noreply.github.com>
2022-09-17 17:56:25 -0700

    Update run_plan.sh

--- .github/workflows/run_plan.sh
+++ .github/workflows/run_plan.sh
@@ -17,7 +17,7 @@ function get_terraform_plan_return_message(){
   getPlanText=$(grep "Plan" 'planMsg.txt')
   planTextResult=$(echo $getPlanText | sed "s/, /,/g")
   
-  echo "line is: $getPlanText"
+  echo "line is: $planTextResult"
   IFS=',' read -a tfArr <<< "$planTextResult"  
   
   toAdd=$(echo ${tfArr[0]} | sed 's/[^0-9]*//g') ;
