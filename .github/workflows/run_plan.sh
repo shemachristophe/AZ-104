937b7488bea093334e68691f9885bcbcfb44cbb3
Shema Christophe <75677615+shemachristophe@users.noreply.github.com>
2022-09-18 22:43:01 -0700

    Update run_plan.sh

--- .github/workflows/run_plan.sh
+++ .github/workflows/run_plan.sh
@@ -46,7 +46,7 @@ function get_terraform_plan_return_message(){
   
   noStateChange="No changes"
   isPlanChanged=$(grep "$noStateChange" 'planMsg.txt')
-  echo "isPlanChanged $isPlanChanged"
+  #echo "isPlanChanged $isPlanChanged"
   #$ echo "anything" | { grep e || true; }
   
   if [ -z "$isPlanChanged" ]
