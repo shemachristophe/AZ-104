e852d6a60eb284a7931f359eff1de6414c8f1ed9
Shema Christophe <75677615+shemachristophe@users.noreply.github.com>
2022-10-30 20:45:50 -0700

    Update run_plan.sh

--- .github/workflows/run_plan.sh
+++ .github/workflows/run_plan.sh
@@ -6,8 +6,6 @@ BRed='\033[1;31m'         # Red
 BGreen='\033[1;32m'       # Green
 BYellow='\033[1;33m'      # Yellow
 BBlue='\033[1;34m'        # Blue
-BPurple='\033[1;35m'      # Purple
-BCyan='\033[1;36m'        # Cyan
 BWhite='\033[1;37m'       # White
 
 function get_terraform_plan_return_message(){
