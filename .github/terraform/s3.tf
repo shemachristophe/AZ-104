53452ad2eb9b1e3203cf8d825fe3b5b991b2ff89
Shema Christophe <75677615+shemachristophe@users.noreply.github.com>
2022-11-10 22:07:46 -0800

    Update s3.tf

--- .github/terraform/s3.tf
+++ .github/terraform/s3.tf
@@ -2,11 +2,11 @@ resource "aws_s3_bucket" "s3Bucket" {
   bucket = "shema-cool-tf-bucket"
   tags = {
     Name = "Shema Bucket"
-    Environment = "dev"
+    Environment = "CI"
   }
 }
 
 resource "aws_s3_bucket_acl" "shema-cool-tf-bucket-acl" {
   bucket = aws_s3_bucket.s3Bucket.id
-  acl    = "public-read"
+  acl    = "private"
 }
